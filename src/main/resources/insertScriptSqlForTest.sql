bf99af59736081f0ed75070ef27634bee672562f
nikolai94 <nikolai@cybermanager.dk>
2017-05-21 10:20:03 +0200

    Integration test and travis setup postgres

--- src/main/resources/insertScriptSqlForTest.sql
+++ src/main/resources/insertScriptSqlForTest.sql
@@ -1,16 +1,14 @@
-/*DELETE FROM public.cityinbook;
+DELETE FROM public.cityinbook;
 DELETE FROM public.books;
 DELETE FROM public.cities;
-*/
 
-/*Insert all test data*/
 INSERT INTO books(id,title, author) VALUES
   (1, 'book1', 'author1'),
   (2, 'book2', 'author2'),
   (3, 'book3', 'author3'),
   (4, 'book4', 'author4');
 
-INSERT INTO cities(id, name, latitude, longtiude, geom) VALUES
+INSERT INTO cities(id, name, latitude, longitude, geom) VALUES
   (1, 'copenhagen', 55.67594,12.56553, ST_GeomFromText('POINT(55.67594 12.56553)', 4326)),
   (2, 'london', 42.98339,-81.23304, ST_GeomFromText('POINT(42.98339 -81.23304)', 4326)),
   (3, 'stockholm', 59.33258,18.0649, ST_GeomFromText('POINT(59.33258 18.0649)', 4326)),
--- src/test/java/DbInterface/FacadeIntegrationTest.java
+++ src/test/java/DbInterface/FacadeIntegrationTest.java
@@ -1,94 +1,113 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package DbInterface;
-//
-//import DbInterface.Facade;
-//import DtoEntity.DtoBookAuthor;
-//import DtoEntity.DtoCity;
-//import entity.Book;
-//import java.io.BufferedReader;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.List;
-//import javax.sql.DataSource;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.junit.Assert.assertThat;
-//import org.mockito.Mockito;
-//import org.postgresql.ds.PGPoolingDataSource;
-//import sql.SqlFacade;
-//
-///**
-// *
-// * @author nikolai
-// */
-//public class FacadeIntegrationTest {
-//
-//    Facade instance;
-//
-//    public FacadeIntegrationTest() {
-//        PGPoolingDataSource datasource = new PGPoolingDataSource();
-//        datasource.setDataSourceName("A Data Source");
-//        datasource.setServerName("188.226.136.226");
-//        datasource.setDatabaseName("test_db");
-//        datasource.setUser("administrator");
-//        datasource.setPassword("firebug");
-//        datasource.setPortNumber(5432);
-//        datasource.setMaxConnections(10);
-//        SqlFacade sql = new SqlFacade(datasource);
-//        
-//    }
-//
-//    @BeforeClass
-//    public static void setUpClass() throws IOException {
-//    ScriptRunner runner = new ScriptRunner(sql.SqlDBConnector.getDBConnection(), false, false);
-//     runner.runScript(new BufferedReader(new FileReader(this.getClass().getResource("/insertScriptSqlForTest.sql"))));
-//    }
-//
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//
-//    @Before
-//    public void setUp() {
-//        instance = new Facade(new SqlFacade(null));
-//        
-//   
-//    }
-//
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of findAllCities method, of class Facade.
-//     */
-//    @Test
-//    public void testFindAllCities() throws IOException, FileNotFoundException {
-//        //Should not be null
-//        BufferedReader in = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/10267.txt")));
-//
-//        //Book book = instance.findAllPossibleCitiesInBook("/home/nikolai/NetBeansProjects/Db_project/src/test/10267.txt");
-//        Book book = instance.findAllPossibleCitiesInBook(in);
-//
-//        assertThat(book.getAuthor(), is("Captain Quincy Allen"));
-//        assertThat(book.getTitle(), is("The Outdoor Chums"));
-//        assertThat(book.getTmpCities().size(), is(17));
-//    }
-//
-//    /* Test
-//        - First we test when the file dosent exists.
-//        - Second we test when the files exists.
-//     */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DbInterface;
+
+import DbInterface.Facade;
+import DtoEntity.DtoBookAuthor;
+import DtoEntity.DtoCity;
+import entity.Book;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.mockito.Mockito;
+import org.postgresql.ds.PGPoolingDataSource;
+import sql.SqlDBConnector;
+import sql.SqlFacade;
+
+/**
+ *
+ * @author nikolai
+ */
+public class FacadeIntegrationTest {
+
+    Facade instance;
+    SqlFacade sql;
+    Connection con;
+    PGPoolingDataSource datasource;
+    public FacadeIntegrationTest() {
+        if (System.getenv("TRAVIS") != null) {
+            datasource = new PGPoolingDataSource();
+            datasource.setDataSourceName("Test travis");
+            datasource.setServerName("127.0.0.1");
+            datasource.setDatabaseName("travis_ci_test");
+            datasource.setUser("travisUser");
+            datasource.setPassword("travisPassfirebug");
+            datasource.setPortNumber(5432);
+        }
+        else{
+            datasource = new PGPoolingDataSource();
+            datasource.setDataSourceName("Test db local");
+            datasource.setServerName("188.226.136.226");
+            datasource.setDatabaseName("test_db");
+            datasource.setUser("administrator");
+            datasource.setPassword("firebug");
+            datasource.setPortNumber(5432);
+        }
+        
+        
+        
+        sql = new SqlFacade(datasource);
+        con = SqlDBConnector.setSource(datasource);
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws IOException, SQLException {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() throws FileNotFoundException, IOException, SQLException {
+        instance = new Facade(sql);
+        ScriptRunner runner = new ScriptRunner(con, false, false);
+        runner.runScript(new BufferedReader(new FileReader(this.getClass().getResource("/insertScriptSqlForTest.sql").getFile())));
+    }
+
+    @After
+    public void tearDown() {
+        datasource.close();
+    }
+
+    /**
+     * Test of findAllCities method, of class Facade.
+     */
+    @Test
+    public void testFindAllCities() throws IOException, FileNotFoundException {
+        //Should not be null
+        BufferedReader in = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/10267.txt")));
+
+        //Book book = instance.findAllPossibleCitiesInBook("/home/nikolai/NetBeansProjects/Db_project/src/test/10267.txt");
+        Book book = instance.findAllPossibleCitiesInBook(in);
+
+        assertThat(book.getAuthor(), is("Captain Quincy Allen"));
+        assertThat(book.getTitle(), is("The Outdoor Chums"));
+        assertThat(book.getTmpCities().size(), is(17));
+    }
+
+    /* Test
+        - First we test when the file dosent exists.
+        - Second we test when the files exists.
+     */
 //    @Test
 //    public void testInsertBooksWithCities() throws IOException {
 //        BufferedReader in = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/10267.txt")));
@@ -101,17 +120,17 @@
 //        boolean resTrue = instance.insertBooksWithCities(this.getClass().getResource("/10267.txt").getFile());
 //        assertThat(resTrue, is(true));
 //    }
-//
+
 //    @Test
 //    public void testinsertBooksWithCitiesHelper() throws IOException {
 //        assertThat(instance.insertBooksWithCitiesHelper(), is(true));
 //    }
-//
-//    @Test
-//    public void getBooksByGeolocationTest() {
-//        List<DtoBookAuthor> result = instance.getBooksByGeolocation((double) 1.1, (double) 2.2);
-//    }
-//
+
+    @Test
+    public void getBooksByGeolocationTest() {
+        List<DtoBookAuthor> result = instance.getBooksByGeolocation((double) 1.1, (double) 2.2);
+    }
+
 //    @Test
 //    public void getBooksByAuthorTest() {
 //        List<DtoBookAuthor> result = instance.getBooksByAuthor("Author");
@@ -121,10 +140,10 @@
 //    public void getCitiesByTitleTest() {
 //        List<DtoCity> result = instance.getCitiesByTitle("title");
 //    }
-//
-//    @Test
-//    public void getBooksByCityTest() {
-//        List<DtoBookAuthor> result = instance.getBooksByCity("city");
-//    }
-//
-//}
+
+    @Test
+    public void getBooksByCityTest() {
+        List<DtoBookAuthor> result = instance.getBooksByCity("city");
+    }
+
+}
